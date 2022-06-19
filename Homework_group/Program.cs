e96e405709f0c1c7191130614b46b417a71d38be
Boris <BorerKicker@yandex.ru>
2022-06-19 17:44:09 +0300

    Added the tasks of the fourth seminar

--- Homework_group/Program.cs
+++ Homework_group/Program.cs
@@ -3,17 +3,52 @@ a = 5; b = 7 -> max = 7
 a = 2; b = 10 -> max = 10
 a = -9; b = -3 -> max = -3 */
 
-Console.Clear();
-Console.Write("Введите первое число: ");
-int basketA = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе число: ");
-int basketB = Convert.ToInt32(Console.ReadLine());
+// Console.Clear();
+// Console.Write("Введите первое число: ");
+// int basketA = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int basketB = Convert.ToInt32(Console.ReadLine());
 
-if (basketA > basketB)
-{
-    Console.WriteLine("max = " + basketA);
-}
-else
-{
-    Console.WriteLine("max = " + basketB);
+// if (basketA > basketB)
+// {
+//     Console.WriteLine("max = " + basketA);
+// }
+// else
+// {
+//     Console.WriteLine("max = " + basketB);
+// }
+
+
+Console.Clear(); 
+Console.Write("Введите пятизначное число: "); 
+ 
+int number = Convert.ToInt32(Console.ReadLine()); 
+int numberTemp = 0; 
+int numberCopy = number; 
+ 
+ 
+ 
+if (number < 10000 || number > 99999) 
+{ 
+    Console.Write("Введено не пятизначное число! Проверьте правильность ввода!"); 
+} 
+ 
+else 
+{ 
+    while (numberCopy > 0) 
+    { 
+        numberTemp = numberTemp * 10 + numberCopy % 10; 
+        numberCopy /= 10; //numberCopy = numberCopy / 10 
+    } 
+ 
+    if (number == numberTemp) 
+    { 
+        Console.WriteLine(number + " - является палиндромом!"); 
+    } 
+ 
+    else 
+    { 
+        Console.WriteLine(number + " - НЕ является палиндромом!"); 
+    } 
+ 
 }
---
+++ fourth seminar/Program.cs
@@ -0,0 +1,75 @@
+﻿using static System.Console;
+Clear();
+
+/* Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5 */
+
+/* 
+Write("Введите число: ");
+int number = Convert.ToInt32(ReadLine());
+int count = 0;
+
+// Вариант решения через while
+
+// while(number > 0)
+// {
+//     number /= 10;
+//     count++;
+// }
+// WriteLine($"В числе цифр - {count}");
+
+// Вариант решения через for
+
+for (int i = number; i > 0; i /= 10)
+{
+    count++;
+}
+WriteLine($"В числе цифр - {count}"); */
+
+// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+/* Write("Введите число N: ");
+int numberN = Convert.ToInt32(ReadLine());
+
+// Вариант решения через while
+
+// int count = 1;
+int work = 1;
+// while (count <= numberN)
+// {
+//     work *= count;
+//     count++;
+// }
+// WriteLine($"Фактариал чмсла {numberN} равен {work}");
+
+// Выриант решения с for
+
+for(int i = 1; i <= numberN; i++)
+{
+    work *= i;
+}
+ WriteLine($"Фактариал чмсла {numberN} равен {work}"); */
+
+/* Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+[1,0,1,1,0,1,0,0] */
+
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(2);
+    if(i < array.Length - 1)
+    {
+        Write($"{array[i]}, ");
+    }
+    else
+    {
+        Write($"{array[i]}.");
+    }
+}
+
+
+
